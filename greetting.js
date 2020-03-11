37a6ab874c212efec67961a5041799811df71b6c
ks2014858052 <48661223+ks2014858052@users.noreply.github.com>
2020-03-11 15:48:04 +0900

    Add files via upload

---
+++ greetting.js
@@ -0,0 +1,16 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input");
+
+const USER_LS = "currentUser";
+
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+}
+
+
+function init() {
+    loadName();
+}
+
+init();
